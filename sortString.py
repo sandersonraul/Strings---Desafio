5ecd3143a5d39a19b619f5dc9f583eb24c6389b5
Raul Oliveira <74179733+sandersonraul@users.noreply.github.com>
2023-01-11 12:42:42 -0300

    Update sortString.py

--- sortString.py
+++ sortString.py
@@ -1,10 +1,10 @@
-n = int(input())
+inputs_number = int(input())
 while n > 0:
-    n -= 1
-    c = input().split()
-    c.sort(key=len, reverse=True)
-    for i in range(len(c)):
-        print(c[i], end = '')
-        if i != len(c)-1:
+    inputs_number -= 1
+    numbers = input().split()
+    numbers.sort(key=len, reverse=True)
+    for i in range(len(numbers)):
+        print(numbers[i], end = '')
+        if i != len(numbers)-1:
             print(' ', end = '')
     print()
