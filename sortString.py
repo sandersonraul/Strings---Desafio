50398fc2c30f52899ad4ba912f914aa0425823ed
sandersonraul <raul.oliveira26@gmail.com>
2023-01-11 14:09:13 -0300

    updating print

--- sortString.py
+++ sortString.py
@@ -1,10 +1,11 @@
 inputs_number = int(input())
-while n > 0:
+a = []
+while inputs_number > 0:
     inputs_number -= 1
     numbers = input().split()
     numbers.sort(key=len, reverse=True)
-    for i in range(len(numbers)):
-        print(numbers[i], end = '')
-        if i != len(numbers)-1:
-            print(' ', end = '')
-    print()
+    a.append(numbers)
+print()
+print()
+for s in a:
+    print(*s)
